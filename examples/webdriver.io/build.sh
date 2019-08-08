c54d26bbb6fb13579feeed8c96376c0d8e1dfe72
Jens Bodal <jensbodal@gmail.com>
2019-08-07 22:59:10 -0700

    properly tag image during build

--- examples/webdriver.io/build.sh
+++ examples/webdriver.io/build.sh
@@ -2,7 +2,7 @@
 pushd "$(dirname $0)"
 
 TARGET_STAGE="${1:-lambci-node10-wdio-test}"
-IMAGE_TAG="al2-node10-chromedriver-wdio-example"
+IMAGE_TAG="al2-node10-chromedriver-wdio-example-${TARGET_STAGE}"
 
 docker build --target $TARGET_STAGE -t $IMAGE_TAG ./
 docker run -it ${IMAGE_TAG} sh
