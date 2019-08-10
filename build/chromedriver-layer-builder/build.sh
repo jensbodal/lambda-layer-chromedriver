ed9ddd6ec64bfb25dbb0fd97a80b10190e85f3b5
Jens Bodal <jensbodal@gmail.com>
2019-08-10 00:23:15 -0700

    Add LICENSE
    allow running entry command in docker build script

--- build/chromedriver-layer-builder/build.sh
+++ build/chromedriver-layer-builder/build.sh
@@ -2,9 +2,14 @@
 pushd "$(dirname $0)"
 
 TARGET_STAGE="${1:-lambda-layer-chromedriver-al2-node10-builder}"
+ENTRY_CMD="$2"
 IMAGE_TAG="lambda-layer-chromedriver-$TARGET_STAGE"
 
 docker build --target $TARGET_STAGE -t $IMAGE_TAG ./
+
+if [ ! -z "${ENTRY_CMD}" ]; then
+  docker run -it $IMAGE_TAG ${ENTRY_CMD}
+fi
 
 popd
 echo $IMAGE_TAG
